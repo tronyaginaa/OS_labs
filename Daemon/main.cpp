dcf8229b22585bcdbaba9c866672cb1e15debf4c
tronyaginaa <tronyaginaa@gmail.com>
2023-10-29 18:37:53 +0300

    add project

---
+++ Daemon/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Daemon
+//
+//  Created by Alexandra on 29.10.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
