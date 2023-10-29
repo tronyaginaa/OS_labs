0d030cac1a0d00817b9d87d0fdd82d98f3614263
tronyaginaa <tronyaginaa@gmail.com>
2023-10-29 18:39:08 +0300

    add all source files

---
+++ Daemon/Daemon.cpp
@@ -0,0 +1,8 @@
+//
+//  Daemon.cpp
+//  Daemon
+//
+//  Created by Alexandra on 29.10.2023.
+//
+
+#include "Daemon.hpp"
---
+++ Daemon/Daemon.hpp
@@ -0,0 +1,13 @@
+//
+//  Daemon.hpp
+//  Daemon
+//
+//  Created by Alexandra on 29.10.2023.
+//
+
+#ifndef Daemon_hpp
+#define Daemon_hpp
+
+#include <stdio.h>
+
+#endif /* Daemon_hpp */
